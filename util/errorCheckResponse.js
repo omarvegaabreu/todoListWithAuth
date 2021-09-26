524fc6f82b1e42208be2deea172988364d13813d
omarvegaabreu <omarvega@hotmail.com>
2021-09-26 14:19:34 -0400

    error check response not working, will not implement

--- util/errorCheckResponse.js
+++ util/errorCheckResponse.js
@@ -1,1 +1,10 @@
+// const express = require("express");
+// const router = express.Router();
 
+// const errorCheckResponse = async (errors) => {
+//   if (!errors.isEmpty()) {
+//     router.res.status(400).json({ errors: errors.array() });
+//   }
+// };
+
+// module.exports = errorCheckResponse;

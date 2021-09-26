887d5c51ab75f2508e039c0f08e5ae71372b37b4
omarvegaabreu <omarvega@hotmail.com>
2021-09-26 14:16:58 -0400

    util error check not working

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator"); //package docs //https://express-validator.github.io/docs/
+const bcrypt = require("bcryptjs"); //https://www.npmjs.com/package/bcryptjs
+const jwt = require("jsonwebtoken"); //  package docs https://jwt.io/
+const jwtSecret = process.env.JWT_SECRET; //secret code for JWT config for auth token
 
 //PUBLIC ROUTE /api/auth
 // .GET user login information
@@ -7,8 +11,15 @@ router.get("/auth", (req, res) =>
   res.send("get user log in info from auth.js")
 );
 
-//.GET once user is logged in, get user information
-router.get("/auth", (req, res) => res.send("once user is logged in auth.js"));
+//.POST once user is logged in, get user information
+router.post(
+  "/auth",
+  [
+    check("email", "Please use a valid email.").isEmail(),
+    check("password", "Passwords is required.").exists(),
+  ],
+  (req, res) => res.send("user is logged in")
+);
 
 //export to server.js
 module.exports = router;

ddcff42e582fc1167cc8ce06a8559136e6b0c6cc
omarvegaabreu <omarvega@hotmail.com>
2021-09-27 11:39:43 -0400

    found error in auth did not import user model

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,12 @@ const { check, validationResult } = require("express-validator"); //package docs
 const bcrypt = require("bcryptjs"); //https://www.npmjs.com/package/bcryptjs
 const jwt = require("jsonwebtoken"); //  package docs https://jwt.io/
 const jwtSecret = process.env.JWT_SECRET; //secret code for JWT config for auth token
+const User = require("../models/User"); //user data
 // const errorCheckResponse = require("../util/errorCheckResponse");
 
 //PUBLIC ROUTE /api/auth
 // .GET user login information
-router.get("/auth", (req, res) =>
-  res.send("get user log in info from auth.js")
-);
+router.get("/", (req, res) => res.send("get user log in info from auth.js"));
 
 //.POST once user is logged in, get user information
 router.post(
@@ -21,11 +20,13 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
-
+    console.log("errors line 22 " + errors);
     //errorCheckResponse(errors, res); //helper function from util folder
 
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res
+        .status(400)
+        .json({ errors: "errors array line 26 " + errors.array() });
     }
 
     const { email, password } = req.body; //destructure req.body object
@@ -34,6 +35,7 @@ router.post(
       // let user = await User.findOne({ email });
 
       let user = await User.find({ email: email });
+      console.log("user auth line 35 " + user);
       if (!user || user.length == 0) {
         return res.status(400).json({ msg: "Invalid credentials" });
       }
@@ -59,6 +61,8 @@ router.post(
           id: user[0]._id,
         },
       };
+
+      console.log("auth line 61" + payload);
       //secret key is required by package manager to generate token
       //this is information that will be sent to the client
       //expires object should be set back for production code to 3600 one hour
@@ -69,7 +73,7 @@ router.post(
         res.json({ token });
       });
     } catch (error) {
-      console.error(error.message);
+      console.error("auth js line 70 " + error.message);
       res.status(500).send("Server error");
     }
   }

ac6ee71a1fc7ea608f8edb2c22def39cfa7d2d8a
omarvegaabreu <omarvega@hotmail.com>
2021-09-28 12:08:55 -0400

    todoSchma done

---
+++ models/Contact.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const moment = require("moment");
+
+const TodoSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  todo: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: moment().format("MM/DD/YYYY"),
+  },
+});
+
+module.exports = mongoose.model("todo", TodoSchema);

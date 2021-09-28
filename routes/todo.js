f91d1954dad4852f64f812faff2078d0cb773a4a
omarvegaabreu <omarvega@hotmail.com>
2021-09-28 17:50:51 -0400

    chg var name from Todo to todoSchema

--- routes/todo.js
+++ routes/todo.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator"); //package docs //https://express-validator.github.io/docs/
 const auth = require("../middleware/auth"); //middleware to protect routes.
 /**Database */
-const Todo = require("../models/Todo"); //todo database
+const TodoSchema = require("../models/Todo"); //todo database
 const User = require("../models/User"); //user database
 const error500 = require("../util/sendError500");
 
@@ -12,7 +12,9 @@ const error500 = require("../util/sendError500");
 //route is private
 router.get("/", auth, async (req, res) => {
   try {
-    const todo = await Todo.find({ user: req.user.id }).sort({ date: -1 });
+    const todo = await TodoSchema.find({ user: req.user.id }).sort({
+      date: -1,
+    });
     res.json(todo);
   } catch (error) {
     console.log("ERROR ON LINE 17 TODO.JS ");
@@ -40,10 +42,10 @@ router.post(
     //errorCheckResponse(errors, res); //from util folder to keep code DRY
     // console.log(req.body);
     const { user, todo, date, todoDescription } = req.body;
-    // console.log(todo);
+    // console.log(req.body);
 
     try {
-      const newTodo = new Todo({
+      const newTodo = new TodoSchema({
         user: req.user.id,
         todo,
         date,
@@ -64,7 +66,38 @@ router.post(
 //route api/todo
 //UPDATE todo
 //route is private
-router.put("/:id", (req, res) => res.send("update todo"));
+router.put("/:id", auth, async (req, res) => {
+  const { user, todo, todoDescription, date } = req.body;
+
+  const todoFields = {};
+  //passes information to todoFields object
+  if (user) todoFields.user = user;
+  if (todo) todoFields.user = user;
+  if (date) todoFields.user = user;
+  if (todoDescription) todoFields.user = user;
+
+  try {
+    let todo = await TodoSchema.findById(req.params.id);
+    if (!todo) return res.status(404).json({ msg: "Todo not found" });
+    //check if user owns account
+    if (todo.user.toString() !== req.user.id) {
+      return res.status(404).json({ msg: "Request not authorized." });
+    }
+
+    todo = await TodoSchema.findByIdAndUpdate(
+      req.params.id,
+      { $set: todoFields },
+      { new: true }
+    );
+
+    res.json({ todo });
+  } catch (error) {
+    console.log("ERROR ON LINE 49 TODO.JS ");
+    console.error(error.message);
+    res.status(500).send("Server error.");
+    //error500(res, error);
+  }
+});
 
 // AUTH REQUIRED NOT PUBLIC ROUTE ./api/todo/:id
 //route api/todo

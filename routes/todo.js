38854dd8a789f684eb98e5ffa6cf86e15b0cf97c
omarvegaabreu <omarvega@hotmail.com>
2021-09-28 12:24:46 -0400

    get contact route

--- routes/todo.js
+++ routes/todo.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator"); //package docs //https://express-validator.github.io/docs/
+const auth = require("../middleware/auth"); //middleware to protect routes.
+/**Database */
+const Todo = require("../models/Todo"); //todo database
+const User = require("../models/User"); //user database
 
 // AUTH REQUIRED NOT PUBLIC ROUTE ./api/todo
 //route api/todo
 //route is private
-router.get("/", (req, res) => res.send("get a todo"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const todo = await Todo.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(todo);
+  } catch (error) {
+    console.log("ERROR ON LINE 17 TODO.JS ");
+    console.error(error.message);
+    res.status(500).send("Server error.");
+  }
+});
 
 // AUTH REQUIRED NOT PUBLIC ROUTE ./api/todo
 //route api/todo

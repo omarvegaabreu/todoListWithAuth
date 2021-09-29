0f858ab0c61090f25f40f76727116e381cdecf60
omarvegaabreu <omarvega@hotmail.com>
2021-09-29 14:48:49 -0400

    TodoState, types and app wired in app state

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,24 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/layout/NavBar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import TodoState from "./context/todo/TodoState";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <NavBar />
-        <div className="container ">
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route exact path="/About" component={About}></Route>
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <TodoState>
+      <Router>
+        <Fragment>
+          <NavBar />
+          <div className="container ">
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route exact path="/About" component={About}></Route>
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </TodoState>
   );
 };
 
---
+++ client/src/context/todo/TodoState.js
@@ -0,0 +1,64 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import TodoContext from "./todoContext";
+import todoReducer from "./todoReducer";
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_TODO,
+  FILTER_TODOS,
+  CLEAR_FILTER,
+  SET_ALERT,
+  REMOVE_ALERT,
+} from "../types";
+
+const TodoState = (props) => {
+  const initialState = {
+    todos: [
+      {
+        id: 1,
+        todo: "first",
+        todoDescription: "first todo",
+      },
+      {
+        id: 2,
+        todo: "second",
+        todoDescription: "second todo",
+      },
+      {
+        id: 3,
+        todo: "third",
+        todoDescription: "third todo",
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
+  //Add todo
+
+  //Delete todo
+
+  //set current todo
+
+  //clear current todo
+
+  //update todo
+
+  //filter todo
+
+  //clear filter
+  return (
+    <TodoContext.Provider
+      value={{
+        todo: state.todo,
+      }}
+    >
+      {props.children}
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoState;

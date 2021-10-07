6c50d2b0c295e6d9761f1037b3560ad85b28b762
omarvegaabreu <omarvega@hotmail.com>
2021-10-07 12:34:28 -0400

    register component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,27 +1,28 @@
-import React from "react";
-import { Button, Checkbox, Form } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Button, Form, Card } from "semantic-ui-react";
 
-const Register = () => (
-  <Form>
-    <Form.Field>
-      <label>First Name</label>
-      <input placeholder="First Name" />
-    </Form.Field>
-    <Form.Field>
-      <label>Last Name</label>
-      <input placeholder="Last Name" />
-    </Form.Field>
-    <Form.Field>
-      <label>email</label>
-      <input placeholder="youremail@email.com" />
-    </Form.Field>
-    <Form.Field>
-      <label>Password</label>
-      <input placeholder="password" />
-    </Form.Field>
+const Register = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-    <Button type="submit">Submit</Button>
-  </Form>
-);
+  const { name, email, password, password2 } = user;
+  return (
+    <Form>
+      <Form.Group widths={1}>
+        <Form.Input label="First name" placeholder="First name" />
+        <Form.Input label="Last name" placeholder="Last name" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Input label="password" placeholder="password" />
+        <Form.Input label="confirm password" placeholder="confirm password" />
+      </Form.Group>
 
+      <Button type="submit">Submit</Button>
+    </Form>
+  );
+};
 export default Register;

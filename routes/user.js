f5baff6032380dd14d31044ba02109acf74860ca
omarvegaabreu <omarvega@hotmail.com>
2021-10-14 20:07:59 -0400

    user route controller done

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User"); //user data
-const { check, validationResult } = require("express-validator"); //package docs //https://express-validator.github.io/docs/
-const bcrypt = require("bcryptjs"); //https://www.npmjs.com/package/bcryptjs
-const jwt = require("jsonwebtoken"); //  package docs https://jwt.io/
-const jwtSecret = process.env.JWT_SECRET; //secret code for JWT config for auth token
-//const error500 = require("../util/sendError500"); helper function to keep code DRY need to implement when refactoring code
-//const errorCheckResponse = require("../util/errorCheckResponse"); need to implement when refactoring code
 
-//PATH TO ENV FILE
-// require("dotenv").config({ path: "../" });
+const { check } = require("express-validator"); //package docs //https://express-validator.github.io/docs/
+
+const userController = require("../Controllers/userController");
 
 //INITIAL PAGE PUBLIC ROUTE /api/users
 
@@ -23,56 +17,7 @@ router.post(
       "Passwords must be at least 6 characters in length."
     ).isLength({ min: 6 }),
   ],
-  async (req, res) => {
-    const errors = validationResult(req); //import express validator returns promise
-
-    //checks res.body for response if error log error array or look at res.json
-
-    if (!errors.isEmpty()) {
-      res.status(400).json({ errors: errors.array() });
-    }
-
-    //errorCheckResponse(errors, res); need to check for errors before implementing
-
-    //user object received in response from mongodb atlas
-    const { name, email, password } = req.body;
-
-    try {
-      let user = await User.findOne({ email });
-
-      if (user) {
-        return res.status(400).json({ msg: "User already exists" });
-      }
-
-      user = new User({
-        name,
-        email,
-        password,
-      });
-
-      const salt = await bcrypt.genSalt(10);
-
-      user.password = await bcrypt.hash(password, salt);
-
-      await user.save();
-
-      //payload is required by package to create token
-      const payload = { user: { id: user.id } };
-      //secret key is required by package manager to generate token
-      //this is information that will be sent to the client
-      //expires object should be set back for production code to 3600 one hour
-      jwt.sign(payload, jwtSecret, { expiresIn: 360000 }, (error, token) => {
-        if (error) {
-          throw error;
-        }
-        res.json({ token });
-      });
-    } catch (error) {
-      // error500(res, error); need to check for errors before implementing
-
-      res.status(500).send("Server error");
-    }
-  }
+  userController.registerUser
 );
 
 //export to server.js

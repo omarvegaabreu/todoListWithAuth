583e2fc8ef4bbfb05d7a0a5b8031317d9683be93
omarvegaabreu <omarvega@hotmail.com>
2022-01-25 12:16:53 -0500

    added export on documentation route

--- routes/documentation.js
+++ routes/documentation.js
@@ -2,5 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) =>
-  res.json("Hello, if you see this message that means this API works!")
+  res.json(
+    "Hello, if you see this message that means this API works in heroku!"
+  )
 );
+
+module.exports = router;

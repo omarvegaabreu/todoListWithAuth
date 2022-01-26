906ab70cbc56bdc33b5532388449d71b7a034a7f
omarvegaabreu <omarvega@hotmail.com>
2022-01-26 17:18:17 -0500

    added more middleware cors

--- server.js
+++ server.js
@@ -17,6 +17,41 @@ mongoDB();
 
 //Init middleware
 app.use(express.json({ extended: false }));
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Pass to next layer of middleware
+  next();
+});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept",
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//   );
+//   next();
+// });
 
 //Disable CORS middleware /***DO NOT MOVE MUST BE BEFORE OF ROUTES */
 // app.use(
@@ -43,21 +78,21 @@ app.use(express.json({ extended: false }));
 //   next();
 // });
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+// app.use(function (req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  next();
-});
+//   next();
+// });
 
 app.use("/api/users", require("./routes/user"));
 app.use("/api/auth", require("./routes/auth"));

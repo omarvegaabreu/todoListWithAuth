ab6a59ff22fa24bc26f06a125c0d077d295dcaf8
omarvegaabreu <omarvega@hotmail.com>
2022-01-26 17:35:15 -0500

    using express headers

--- server.js
+++ server.js
@@ -17,31 +17,40 @@ mongoDB();
 
 //Init middleware
 app.use(express.json({ extended: false }));
+
 app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // Pass to next layer of middleware
   next();
 });
+// app.use(function (req, res, next) {
+// Website you wish to allow to connect
+// res.setHeader("Access-Control-Allow-Origin", "*");
+
+// Request methods you wish to allow
+// res.setHeader(
+//   "Access-Control-Allow-Methods",
+//   "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+// );
+
+// Request headers you wish to allow
+// res.setHeader(
+// "Access-Control-Allow-Headers",
+//"X-Requested-With,content-type"
+// );
+
+// Set to true if you need the website to include cookies in the requests sent
+// to the API (e.g. in case you use sessions)
+// res.setHeader("Access-Control-Allow-Credentials", true);
+
+// res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+// Pass to next layer of middleware
+//next();
+//});
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.header(

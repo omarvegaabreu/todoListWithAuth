664e9b41e7409e6050b9daf4eba2e85381b1d2f8
omarvegaabreu <omarvega@hotmail.com>
2022-01-26 13:06:26 -0500

    added cors options for single website

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(express.json({ extended: false }));
 //   );
 //   next();
 // });
+
 const whitelist = ["https://zen-cori-46710b.netlify.app/"];
 const corsOptions = {
   origin: function (origin, callback) {
@@ -52,10 +53,19 @@ app.use(cors(corsOptions));
 // });
 // res.set("Access-Control-Allow-Origin", "https://zen-cori-46710b.netlify.app/");
 //API ROUTES
-app.use("/api/documentation", require("./routes/documentation")); //to check it works
-app.use("/api/users", require("./routes/user"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/todos", require("./routes/todo"));
+// //app.use(
+//   "/api/documentation",
+//   cors(corsOptions),
+//   require("./routes/documentation")
+// ); //to check it works
+
+app.use("/api/users", cors(corsOptions), require("./routes/user"));
+app.use("/api/auth", cors(corsOptions), require("./routes/auth"));
+app.use("/api/todos", cors(corsOptions), require("./routes/todo"));
+
+// app.use("/api/users", require("./routes/user"));
+// app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/todos", require("./routes/todo"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {

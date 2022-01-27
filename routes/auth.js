e2d568e4eeccd3d1c81c0a4a669216a5eaf2e951
omarvegaabreu <omarvega@hotmail.com>
2022-01-27 11:36:08 -0500

    added cors library to auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,16 @@ const router = express.Router();
 const { check } = require("express-validator"); //package docs //https://express-validator.github.io/docs/
 const auth = require("../middleware/auth");
 const authRouteController = require("../Controllers/authController.js");
+const cors = require("cors");
 
 //PUBLIC ROUTE /api/auth
 // .GET validate logged in user
-router.get("/", auth, authRouteController.getUser);
+router.get("/", cors(), auth, authRouteController.getUser);
 
 //.POST once user is logged in, get user information
 router.post(
   "/",
+  cors(),
   [
     check("email", "Please use a valid email.").isEmail(),
     check("password", "Passwords is required.").exists(),
